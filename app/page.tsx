007cdf0202b889ef2e599932759da52e5e1fbacd
George Bockari <bitwhys@gmail.com>
2023-03-09 00:33:33 -0500

    refactor: add branding colors

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,17 @@ const PostCard = ({ date, title, published }) => {
   return (
     <article className="flex max-w-xl flex-col items-start justify-between">
       <div className="flex items-center gap-x-4 text-xs">
-        <time dateTime="2020-03-16" className="text-gray-500">
+        <time dateTime="2020-03-16" className="text-neutralDark-10">
           {date}
         </time>
         <a
           href="#"
-          className="relative z-10 rounded-full bg-gray-50 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100"
+          className="relative z-10 rounded-full bg-neutralDark-2 py-1.5 px-3 font-medium text-neutralDark-10 hover:bg-neutralDark-3"
         >
           {published ? 'Published' : 'Draft'}
         </a>
       </div>
-      <h3 className="mt-3 font-heading text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+      <h3 className="mt-3 font-heading text-lg font-normal leading-6 tracking-wider group-hover:text-neutralDark-11">
         <a href="#">
           <span className="absolute inset-0" />
           {title}
@@ -25,14 +25,14 @@ const PostCard = ({ date, title, published }) => {
 }
 export default function Home() {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl">
-          <h2 className="font-heading text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
-          <p className="mt-2 text-lg leading-8 text-gray-600">
+          <h2 className="font-heading text-3xl font-bold tracking-tight sm:text-4xl">From the blog</h2>
+          <p className="mt-2 text-lg leading-8 text-neutralDark-11">
             Learn how to grow your business with our expert advice.
           </p>
-          <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
+          <div className="mt-10 space-y-16 border-t border-neutralDark-6 pt-10 sm:mt-16 sm:pt-16">
             {allPosts.map((post, i) => (
               <PostCard key={post.date} date={post.date} title={post.title} published={post.published ?? undefined} />
             ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,40 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const { radixToTailwind } = require('./lib/radixToTailwind')
+const {
+  olive,
+  oliveDark,
+  lime,
+  limeDark,
+  skyDark,
+  sky,
+  amber,
+  amberDark,
+  grass,
+  grassDark,
+  red,
+  redDark,
+} = require('@radix-ui/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['app/**/*.{ts,tsx}'],
   theme: {
     extend: {
+      colors: {
+        neutral: radixToTailwind(olive),
+        neutralDark: radixToTailwind(oliveDark),
+        primary: radixToTailwind(lime),
+        primaryDark: radixToTailwind(limeDark),
+        info: radixToTailwind(sky),
+        infoDark: radixToTailwind(skyDark),
+        warning: radixToTailwind(amber),
+        warningDark: radixToTailwind(amberDark),
+        success: radixToTailwind(grass),
+        successDark: radixToTailwind(grassDark),
+        danger: radixToTailwind(red),
+        dangerDark: radixToTailwind(redDark),
+      },
+
       fontFamily: {
         sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
         heading: ['var(--font-heading)', ...defaultTheme.fontFamily.sans],
