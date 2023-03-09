3b527bbce68afbef43dea4b2107434d113cfd0b4
George Bockari <bitwhys@gmail.com>
2023-03-09 00:20:57 -0500

    feat: map all post to frontpage feed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+import { allPosts } from 'contentlayer/generated'
+
+const PostCard = ({ date, title, published }) => {
+  return (
+    <article className="flex max-w-xl flex-col items-start justify-between">
+      <div className="flex items-center gap-x-4 text-xs">
+        <time dateTime="2020-03-16" className="text-gray-500">
+          {date}
+        </time>
+        <a
+          href="#"
+          className="relative z-10 rounded-full bg-gray-50 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100"
+        >
+          {published ? 'Published' : 'Draft'}
+        </a>
+      </div>
+      <h3 className="mt-3 font-heading text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+        <a href="#">
+          <span className="absolute inset-0" />
+          {title}
+        </a>
+      </h3>
+    </article>
+  )
+}
 export default function Home() {
   return (
     <div className="bg-white py-24 sm:py-32">
@@ -8,49 +33,9 @@ export default function Home() {
             Learn how to grow your business with our expert advice.
           </p>
           <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
-            <article className="flex max-w-xl flex-col items-start justify-between">
-              <div className="flex items-center gap-x-4 text-xs">
-                <time dateTime="2020-03-16" className="text-gray-500">
-                  Mar 16, 2020
-                </time>
-                <a
-                  href="#"
-                  className="relative z-10 rounded-full bg-gray-50 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100"
-                >
-                  Marketing
-                </a>
-              </div>
-              <div className="group relative">
-                <h3 className="mt-3 font-heading text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                  <a href="#">
-                    <span className="absolute inset-0" />
-                    Boost your conversion rate
-                  </a>
-                </h3>
-                <p className="mt-5 text-sm leading-6 text-gray-600 line-clamp-3">
-                  Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo
-                  necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta
-                  laboris incididunt.
-                </p>
-              </div>
-              <div className="relative mt-8 flex items-center gap-x-4">
-                <img
-                  src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                  className="h-10 w-10 rounded-full bg-gray-50"
-                />
-                <div className="text-sm leading-6">
-                  <p className="font-semibold text-gray-900">
-                    <a href="#">
-                      <span className="absolute inset-0" />
-                      Michael Foster
-                    </a>
-                  </p>
-                  <p className="text-gray-600">Co-Founder / CTO</p>
-                </div>
-              </div>
-            </article>
-            {/* More posts... */}
+            {allPosts.map((post, i) => (
+              <PostCard key={post.date} date={post.date} title={post.title} published={post.published ?? undefined} />
+            ))}
           </div>
         </div>
       </div>
